9afc0e8cae99e6e0e126ac71e5c4e02996f5a2e0
winsxx <13512071@std.stei.itb.ac.id>
2015-04-19 16:10:22 +0700

    Cancel offer, fetch item, offer, send accept, and signup response done

---
+++ PhilosopherStoneClient/src/philosopherstoneclient/connection/response/SendAcceptResponse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package philosopherstoneclient.connection.response;
+
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author winsxx
+ */
+public class SendAcceptResponse extends Response{
+
+    public SendAcceptResponse(String jsonString) throws ParseException, ResponseFailException, ResponseErrorException {
+        super(jsonString);
+    }
+    
+}

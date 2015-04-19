485c77dcf3e0ce86f59f37e959b83eb3eec38241
winsxx <13512071@std.stei.itb.ac.id>
2015-04-19 11:47:13 +0700

    JsonSocket dan SignUpRequest done. Both are tested.

--- PhilosopherStoneClient/src/philosopherstoneclient/PhilosopherStoneClient.java
+++ PhilosopherStoneClient/src/philosopherstoneclient/PhilosopherStoneClient.java
@@ -6,6 +6,12 @@
 
 package philosopherstoneclient;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import philosopherstoneclient.connection.JsonSocket;
+import philosopherstoneclient.connection.request.SignUpRequest;
+
 /**
  *
  * @author winsxx
@@ -16,7 +22,48 @@ public class PhilosopherStoneClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SignUpRequest sur = new SignUpRequest();
+        sur.password = "1434325";
+        sur.username = "kelvahn";
+        
+        String json = sur.toJsonString();
+        System.out.println(json);
+        
+        JsonSocket js = new JsonSocket("167.205.32.46", 8025, 5000);
+        for(int i=0; i<3; i++){
+            try {
+                System.out.println("Start write");
+                js.connect();
+                System.out.println("Done connect");
+            } catch (IOException ex) {
+                System.out.println("fail connect");
+                Logger.getLogger(PhilosopherStoneClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                System.out.println("Start write");
+                js.write(json);
+                System.out.println("Done write");
+            } catch (IOException ex) {
+                System.out.println("fail write");
+                Logger.getLogger(PhilosopherStoneClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String output;
+            try {
+                System.out.println("Start read");
+                output = js.read();
+                System.out.println("Done read");
+                System.out.println(output);
+            } catch (IOException ex) {
+                System.out.println("fail read");
+                Logger.getLogger(PhilosopherStoneClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                js.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PhilosopherStoneClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println("Done");
     }
     
 }
---
+++ PhilosopherStoneClient/src/philosopherstoneclient/connection/request/SignUpRequest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package philosopherstoneclient.connection.request;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.json.simple.JSONValue;
+
+/**
+ *
+ * @author winsxx
+ */
+public class SignUpRequest extends Request{
+    public String username;
+    public String password;
+    
+    public SignUpRequest() {
+        method = "signup";
+    }
+    
+    @Override
+    public String toJsonString() {
+        Map obj = new LinkedHashMap();
+        
+        obj.put("method", method);
+        obj.put("username", username);
+        obj.put("password", password);
+        
+        String jsonString = JSONValue.toJSONString(obj);
+        
+        return jsonString;
+    }
+    
+}

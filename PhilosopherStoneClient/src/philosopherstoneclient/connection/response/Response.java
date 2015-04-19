1e551caa956daa83848c89b6fed3bc31e3e06c6b
winsxx <13512071@std.stei.itb.ac.id>
2015-04-19 15:58:28 +0700

    Response abstract class and exception

---
+++ PhilosopherStoneClient/src/philosopherstoneclient/connection/response/Response.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package philosopherstoneclient.connection.response;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author winsxx
+ */
+public abstract class Response {
+    protected JSONObject responseJson;
+    
+    protected Response(String jsonString) throws ParseException, ResponseFailException, ResponseErrorException{
+        JSONParser jsonParser = new JSONParser();
+        responseJson = (JSONObject) jsonParser.parse(jsonString);
+        
+        String status = (String) responseJson.get("status");
+        if(!status.equals("ok")){
+            if (status.equals("fail")){
+                String description = (String) responseJson.get("description");
+                throw new ResponseFailException(description);
+            }else{
+                throw new ResponseErrorException();
+            }
+        }
+    }
+    
+}
